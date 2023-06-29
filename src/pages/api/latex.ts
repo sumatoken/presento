8461778cd7a7ed6f7340f6ff4bc01d8d38f88d81
Mohammed Bermime <bermime1@gmail.com>
2023-06-30 00:56:46 +0100

    Refactoring

--- src/pages/api/latex.ts
+++ src/pages/api/latex.ts
@@ -1,9 +1,18 @@
+import { openai } from "@/lib/openai";
 import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { exit } from "process";
 import { promisify } from "util";
 const exec = promisify(require("child_process").exec);
 
-async function compileLaTeX(documentPath: string): Promise<string> {
+const generateLatexDocument = async (document: string) => {
+  const tempFilePath = "/tmp/document.tex";
+  fs.writeFileSync(tempFilePath, document);
+  console.log("Wrote LaTeX file to", tempFilePath);
+  return tempFilePath;
+};
+
+const compileLaTeX = async (documentPath: string): Promise<string> => {
   const { stdout, stderr } = await exec(
     `pdflatex -output-directory=/tmp ${documentPath}`
   );
@@ -12,7 +21,24 @@ async function compileLaTeX(documentPath: string): Promise<string> {
   }
   console.log(stdout);
   return "/tmp/document.pdf";
-}
+};
+
+const generateHeader = async (context: string) => {
+  const defaultHeader = `\\title{Empathy Mapping for Binder App}
+        \\subtitle{Understanding our Users}
+        \\date{\\today}
+        \\author{Mohammed Bermime}
+        \\institute{}`;
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Modify the following Latex code (make sure to escape using backslashes) using the context below:
+      ${defaultHeader}.
+      Context: ${context}`,
+    max_tokens: 1000,
+  });
+
+  return { text: completion.data.choices[0].text as string };
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -64,16 +90,17 @@ In this presentation, we will discuss the empathy mapping of our two user types 
 \\section{Empathy Map for Businesses}
 \\begin{frame}{Think and Feel}
 Concerned about reaching more customers, excited about the potential for growth, worried about maintaining customer loyalty, curious about digital marketing and its benefits.
-\\end{frame}`;
+    \\end{frame}`;
 
-  document = `${preamble}\n${document}\n${postamble}`;
+  const { text } = await generateHeader(
+    "Presenatioin about an app that I'm building. It's called Label and it would revolutionize the way we label things. My name is Said Hamid from the univerity of Ben M'Sik."
+  );
+  document = `${preamble}\n${text}\n\\begin{document}\\textbf{Hello World!}${postamble}`;
 
-  const tempFilePath = "/tmp/document.tex";
-  fs.writeFileSync(tempFilePath, document);
-  console.log("Wrote LaTeX file to", tempFilePath);
-  // Compiling the LaTeX file to PDF using pdflatex
+  const latexPath = await generateLatexDocument(document);
+
   try {
-    const pdfPath = await compileLaTeX(tempFilePath);
+    const pdfPath = await compileLaTeX(latexPath);
     const pdf = fs.readFileSync(pdfPath);
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Disposition", "attachment; filename=document.pdf");

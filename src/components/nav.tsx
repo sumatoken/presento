92cc50bad7a16133df8299c846dafdd327b08297
Mohammed Bermime <bermime1@gmail.com>
2023-07-02 18:17:11 +0100

    Adding shadcn for complex components

---
+++ src/components/nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div className="w-[1440px] h-[72px] relative">
+            <div className="w-[1440px] h-[72px] left-0 top-0 absolute bg-stone-900" />
+
+        </div>
+    )
+}
+
+
+{/* <div className="left-[189px] top-[27px] absolute text-center text-red-400 text-[16px] font-normal">Home</div>
+            <div className="left-[323px] top-[27px] absolute text-center text-red-400 text-[16px] font-normal">Create</div>
+            <div className="w-[75px] h-9 left-[1300px] top-[19px] absolute">
+                <div className="w-[75px] h-9 left-0 top-0 absolute bg-stone-700 rounded" />
+                <div className="left-[14px] top-[8px] absolute text-center text-red-400 text-[16px] font-normal">Log in</div>
+            </div> */}
--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/nav";
 import { trpc } from "@/utils/trpc";
 
 export default function GeneratePage() {
@@ -5,13 +6,8 @@ export default function GeneratePage() {
 
     return (
         <div>
+            <Nav />
 
-            <h1>Hello from generate</h1>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={
-                () => {
-                    latex.mutate({ slides: ["Hello baby", "Hello baby 2"] })
-                }
-            }>Generate PDF</button>
         </div>
     );
 }

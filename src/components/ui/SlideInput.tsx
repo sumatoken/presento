bfdc189671c75bb3cc60338947c4b8c4cb477778
Mohammed Bermime <bermime1@gmail.com>
2023-07-04 12:08:00 +0100

    Implemented simple carousel

--- src/components/ui/SlideInput.tsx
+++ src/components/ui/SlideInput.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
     description: string
     index: number
     value: string
-    name?: string
+    name: string
     onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
-export default function SlideInput({ description, index, name, onChange }: Props) {
+export default function SlideInput({ description, name, value, onChange }: Props) {
+
     return (
         <div className="w-1/3 h-[255px] flex flex-col bg-[#E7D4C0] p-4 rounded">
             <span className="text-center text-stone-900 text-[12px] font-bold p-2">
                 {description}
             </span>
-            <textarea className="p-2.5 w-full h-[200px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 " name={name} placeholder="Write here..." onChange={onChange} />
+            <textarea className="p-2.5 w-full h-[200px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Write here..." name={name} value={value} onChange={onChange} />
         </div>
     )
 }
--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,17 +1,17 @@
 import Nav from "@/components/nav";
 import Button from "@/components/ui/Button";
 import SlideInputGroup from "@/components/ui/SlideInputGroup";
-import SlideInput from "@/components/ui/slideInput";
 import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 
 export default function GeneratePage() {
     const latex = trpc.latex.generatePDF.useMutation()
+    const [numberOfSlides, setNumberOfSlides] = useState(3) // because counting starts at 0
     const [inputs, setInputs] = useState<{
         context: string,
         prompt: string,
     }[]>([])
-    const [numberOfSlides, setNumberOfSlides] = useState(4)
+    const [[slide, direction], setSlide] = useState([0, 0])
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>, index: number) => {
         const { value } = e.target;
         const name = e.target.name as "context" | "prompt";
@@ -30,7 +30,18 @@ export default function GeneratePage() {
         //latex.mutate({ slides: inputs })
         console.log(inputs)
     }
-
+    const goToNextSlide = () => {
+        if (slide + 1 > numberOfSlides) {
+            return;
+        }
+        setSlide([slide + 1, 1])
+    }
+    const goToPreviousSlide = () => {
+        if (slide <= 0) {
+            return;
+        }
+        setSlide([slide - 1, -1])
+    }
     return (
         <div>
             <Nav />
@@ -44,15 +55,11 @@ export default function GeneratePage() {
                     Starting of with the context of the presentation:
                 </span>
             </div>
-            {
-                [...Array(numberOfSlides)].map((_, index) =>
-                    <SlideInputGroup key={index} index={index} inputs={inputs} handleChange={handleChange} />
-                )
-            }
+            <SlideInputGroup key={slide} index={slide} value={inputs[slide] || { context: "", prompt: "" }} handleChange={handleChange} />
 
             <div className="p-4 flex flex-row gap-12 justify-center items-center  ">
-                <Button value="Back" variant="back" />
-                <Button value="Next" variant="next" onClick={e => handleSubmit(e)} />
+                <Button value="Back" variant="back" onClick={goToPreviousSlide} />
+                <Button value="Next" variant="next" onClick={goToNextSlide} />
             </div>
         </div>
     );
